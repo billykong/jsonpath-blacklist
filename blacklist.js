0a4568312aa1d2f5cffc829b7b1ff696aa00eb15
Billy Kong <billyklh@gmail.com>
2018-06-19 15:10:08 +0800

    initial commit

---
+++ blacklist.js
@@ -0,0 +1,19 @@
+function toBlacklist(blacklist, inputJSON) {
+  if (blacklist) {
+    outputJSON = blacklist.reduce((json, rule) => {
+      rule.split(".").reduce((json, fragment, currentIndex, array) => {
+        let isLastFragment = currentIndex == (array.length - 1);
+        let arrayMatches = fragment.match(/^([^\[\]]+)\[(\d+)\]/);
+        if (arrayMatches) {
+          return isLastFragment ? json[arrayMatches[1]].splice(arrayMatches[2]) : json[arrayMatches[1]][arrayMatches[2]];
+        } else {
+          return isLastFragment ? (json[fragment] = undefined) : json[fragment];
+        }
+      }, json);
+      return json;
+    }, inputJSON)
+  }
+  return outputJSON;
+}
+
+module.exports = toBlacklist;
