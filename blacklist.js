f4573e9ac116e12d0debc6da572bcdbceeb32195
Billy Kong <billyklh@gmail.com>
2018-06-22 10:25:08 +0800

    replace unwanted values with undefined instead of removal to prevent array index integrity

--- blacklist.js
+++ blacklist.js
@@ -5,7 +5,7 @@ function toBlacklist(blacklist, inputJSON) {
         let isLastFragment = currentIndex == (array.length - 1);
         let arrayMatches = fragment.match(/^([^\[\]]+)\[(\d+)\]/);
         if (arrayMatches) {
-          return isLastFragment ? json[arrayMatches[1]].splice(arrayMatches[2]) : json[arrayMatches[1]][arrayMatches[2]];
+          return isLastFragment ? (json[arrayMatches[1]][arrayMatches[2]] = undefined) : json[arrayMatches[1]][arrayMatches[2]];
         } else {
           return isLastFragment ? (json[fragment] = undefined) : json[fragment];
         }
