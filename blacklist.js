e174b0509b1303c80c254718b2670296576a34cd
Billy Kong <billyklh@gmail.com>
2018-06-25 23:30:55 +0800

    handle null values in array

--- blacklist.js
+++ blacklist.js
@@ -5,7 +5,12 @@ function toBlacklist(blacklist, inputJSON) {
         let isLastFragment = currentIndex == (array.length - 1);
         let arrayMatches = fragment.match(/^([^\[\]]+)\[(\d+)\]/);
         if (arrayMatches) {
-          return isLastFragment ? (json[arrayMatches[1]][arrayMatches[2]] = undefined) : json[arrayMatches[1]][arrayMatches[2]];
+          if (isLastFragment) {
+            json[arrayMatches[1]][arrayMatches[2]] = undefined;
+            json[arrayMatches[1]] = json[arrayMatches[1]].filter(v => v); //remove undefined/null values from array
+          } else {
+            return json[arrayMatches[1]][arrayMatches[2]];
+          }
         } else {
           return isLastFragment ? (json[fragment] = undefined) : json[fragment];
         }
